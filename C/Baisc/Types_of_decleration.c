bc812a6e4ac4eec7be6ef63f446b2660da6083f7
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-02-27 01:55:02 +0530

    this is a tyep declaration in c

---
+++ C/Baisc/Types_of_decleration.c
@@ -0,0 +1,36 @@
+// TYpes of decleration
+// author: Mukul
+// date: 2021-04-10 01:00:00
+// description: There are two types of decleration in C programming language. They are:
+// 1. type decleration
+// 2. arthemetic decleration
+// condional decleration
+#include <stdio.h>
+int main(){
+    //  type instruction
+    int city_code;
+    float salary ;
+    char grade ;
+
+
+    // arthemetic instruction
+    int num = 10;
+    float pi = 3.14;
+    double largeNum = 12345.6789;
+    char letter = 'A';
+
+    printf("Integer: %d\n", num);
+    printf("Float: %.2f\n", pi);
+    printf("Double: %.4lf\n", largeNum);
+    printf("Character: %c\n", letter);
+
+    // control instruction
+    // sequence control instruction 
+    // selection control instruction
+    // Repetition or loop ccntrol instruction
+    // case control instruction
+    
+
+    
+    return 0 ; 
+}
